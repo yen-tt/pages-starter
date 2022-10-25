e9e93d0354464878bbc65d34e43169550ffba295
Yen Truong <ytruong@yext.com>
2022-10-25 15:43:34 -0400

    update npm commands for testing pages build command

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
+// for testing:
+// {
+//   projectStructureConfig: {
+//     filepathsConfig: {
+//       templatesRoot: 'test',
+//       scope: 'someScope'
+//     }
+//   }
+// }
+
 export default defineConfig({
   plugins: [react(), yextSSG()],
 });
